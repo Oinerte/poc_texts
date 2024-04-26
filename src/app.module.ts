a899bf513dd7dc1e74c8e822493ddb31fae36632
Oinerte <lauro.meireles@germinare.org.br>
2024-04-26 13:31:11 -0300

    changing mongo url

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,9 @@ import { CatText, CatTextSchema } from './schemas/cat-texts.schema';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27018/'),
+    MongooseModule.forRoot(
+      'mongodb+srv://user_monguinho:senha_monguinho@monguinho.ptrpqhx.mongodb.net/?retryWrites=true&w=majority&appName=monguinho',
+    ),
     MongooseModule.forFeature([{ name: CatText.name, schema: CatTextSchema }]),
   ],
   controllers: [AppController],
